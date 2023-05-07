f32344c25a9513bad4a6e34d8d3cbb020755acaa
Haitam Aouad Idrissi <70385849+statiqueplasma@users.noreply.github.com>
2023-05-07 15:49:49 +0200

    Update urls.py
    updated default url() to re_path() as url() is marked deprecated.

--- raster/urls.py
+++ raster/urls.py
@@ -1,36 +1,36 @@
-from django.conf.urls import url
+from django.urls import re_path
 from raster.views import AlgebraView, ExportView, LegendView
 
 urlpatterns = [
 
     # Normal raster tiles endpoint
-    url(
+    re_path(
         r'^tiles/(?P<layer>[^/]+)/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>png|jpg|tif)$',
         AlgebraView.as_view(),
         name='tms',
     ),
 
     # Raster algebra endpoint
-    url(
+    re_path(
         r'^algebra/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+).(?P<frmt>jpg|png|tif)$',
         AlgebraView.as_view(),
         name='algebra',
     ),
 
     # Pixel value endpoint
-    url(
+    re_path(
         r'^pixel/(?P<xcoord>-?\d+(?:\.\d+)?)/(?P<ycoord>-?\d+(?:\.\d+)?)$',
         AlgebraView.as_view(),
         name='pixel',
     ),
 
     # Raster legend endpoints.
-    url(
+    re_path(
         r'^legend$',
         LegendView.as_view(),
         name='legend',
     ),
-    url(
+    re_path(
         r'^legend/(?P<legend_id>[^/]+)$',
         LegendView.as_view(),
         name='legend-detail',
